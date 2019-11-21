ce0b9d4e9f6515951abcd0d5615b68f61214d7e2
Nika Barsukov <nika.barsukov@ol.ge>
2019-11-21 22:47:58 +0400

    message

--- src/public/assets/script/script.js
+++ src/public/assets/script/script.js
@@ -73,31 +73,57 @@ class CategoryTableController {
         this.blurMode = false;
 
         this.init();
+        this.hangEvents();
     }
     init = () => {
+        this.addButtonRef = document.getElementById('categories-add');
+        this.editButtonRef = document.getElementById('categories-edit');
+        this.removeButtonRef = document.getElementById('categories-remove');
+        this.refreshButtonRef = document.getElementById('categories-refresh');
         this.tableBodyRef = document.getElementById('categories-table-body');
+    };
+    hangEvents = () => {
+
+    };
+    selectRow = (rowIndex) => {
+        const rows = this.tableBodyRef.getElementsByTagName('admin-table-row');
+        const row = this.tableBodyRef.getElementsByTagName('admin-table-row')[rowIndex];
+
+        this.selectedRowIndex = rowIndex;
+
+        for (const _row of rows) {
+            if (_row === row) {
+                row.className += ' selected';
+            } else {
+                row.classList.remove('selected');
+            }
+        }
     };
     refresh = () => {
         HTTP.get(window.location.href + '/category/get')
-            .then((payload) => this.render(payload))
+            .then((payload) => {
+                this.tableData = payload;
+                this.render();
+            })
             .catch((error) => console.error(error));
     };
-    render = (payload) => {
+    render = () => {
         this.tableBodyRef.innerHTML = null;
 
-        for (let i = 0; i < payload.length; i++) {
-            this.tableBodyRef.innerHTML += `
-                <admin-table-row>
-                    <label>#${i}</label>
-                    <label>${payload[i].name}</label>
-                    <label>${payload[i].slug}</label>
-                    <label>${payload[i].content}</label>
-                    <label>${payload[i].views}</label>
-                    <label>${new Date(payload[i].timestamp).toLocaleDateString()}</label>
-                    <label>${payload[i].status === true ? 'On' : 'Off'}</label>
-                    <label>${payload[i].trash === true ? 'Removed' : 'Active'}</label>
-                </admin-table-row>
+        for (let i = 0; i < this.tableData.length; i++) {
+            const adminTableRow = document.createElement('admin-table-row');
+            adminTableRow.innerHTML = `
+                <label>#${i}</label>
+                <label>${this.tableData[i].name}</label>
+                <label>${this.tableData[i].slug}</label>
+                <label>${this.tableData[i].content}</label>
+                <label>${this.tableData[i].views}</label>
+                <label>${new Date(this.tableData[i].timestamp).toLocaleDateString()}</label>
+                <label>${this.tableData[i].status === true ? 'On' : 'Off'}</label>
+                <label>${this.tableData[i].trash === true ? 'Removed' : 'Active'}</label>
             `;
+            adminTableRow.addEventListener('click', this.selectRow.bind(this, i), true);
+            this.tableBodyRef.appendChild(adminTableRow);
         }
     };
 }
