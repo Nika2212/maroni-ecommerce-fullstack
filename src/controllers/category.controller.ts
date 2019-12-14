7325e62ea6af3eb4a61e5266617705a15c23c1de
Nika Barsukov <nika.barsukov@ol.ge>
2019-12-14 15:42:15 +0400

    New architecture

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,73 +1,18 @@
-import { BaseController } from "../core/base.controller";
-import { IBaseController } from "../core/IBase.controller";
-import { Request, Response } from "express";
-import { Category } from "../models/category.model";
-import { Manufacturer } from "../models/manufacturer.model";
-import { Series } from "../models/series.model";
+import { BaseController } from '../core/base.controller';
+import { IBaseController } from '../core/IBase.controller';
+import { Request, Response } from 'express';
+import { Category } from '../models/category.model';
+import { Document } from 'mongoose';
 
 export class CategoryController extends BaseController implements IBaseController {
-    public async get(req: Request, res: Response): Promise<Response | void> {
-        const categoryArray = await Category.find({}).exec();
-
-        res.status(200).send(categoryArray);
-    }
-    public async getOne(req: Request, res: Response): Promise<Response | void> {
-        const categoryName = req.params.categoryName;
-
-        try {
-            const categoryItem = await Category.findOne({name: categoryName});
-
-            return res.status(200).send(categoryItem);
-        } catch (e) {
-            return res.status(404).send('Category not found.');
-        }
-    }
+    public async get(req: Request, res: Response): Promise<Response | void> {}
+    public async getOne(req: Request, res: Response): Promise<Response | void> {}
     public async create(req: Request, res: Response): Promise<Response | void> {
-        const { name, slug, content } = req.body;
-
-        // TODO New category fields validation
-        if (await Category.findOne({name})) {
-            res.status(409).send('Category already exists.');
-        }
-
-        const newCategory = new Category({
-            name,
-            slug,
-            content
-        });
-
-        await newCategory.save();
-
-        return res.status(200).send(newCategory);
+        // TODO Validation request body
+        Category.createCategory(req.body)
+            .then((category: Document) => res.status(200).send(category))
+            .catch(e => res.status(409).send(e.message));
     }
-    public async update(req: Request, res: Response): Promise<Response | void> {
-        const categoryID = req.params.categoryID;
-        const categoryBody = req.body;
-
-        // TODO Validate category fields
-
-        if (!await Category.findById(categoryID)) res.status(404).send('Category not found');
-
-        delete categoryBody.views;
-        delete categoryBody.timestamp;
-
-        try {
-            await Category.findByIdAndUpdate(categoryID, categoryBody);
-
-            res.status(200).send(await Category.findById(categoryID));
-        } catch (e) {
-            res.status(500).send(e);
-        }
-    }
-    public async delete(req: Request, res: Response): Promise<Response | void> {
-        const categoryID = req.params.categoryID;
-
-        try {
-            await Category.findByIdAndRemove(categoryID);
-
-            res.status(200).send('Category removed successfully')
-        } catch (e) {
-            res.status(500).send(e);
-        }
-    }
+    public async update(req: Request, res: Response): Promise<Response | void> {}
+    public async delete(req: Request, res: Response): Promise<Response | void> {}
 }
--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,39 +1,80 @@
-import mongoose, {Schema} from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Document, Schema } from 'mongoose';
 
-const CategorySchema = new mongoose.Schema({
+const MongoCategorySchema = new mongoose.Schema({
     name: {
         type: String,
+        min: 3,
         max: 255,
         required: true
     },
     status: {
         type: Boolean,
-        default: true,
-    },
-    slug: {
-        type: String,
-        max: 255,
         required: false,
-        default: '',
-    },
-    content: {
-        type: String,
-        max: 255,
-        default: '',
-    },
-    views: {
-        type: Number,
-        default: 0,
+        default: true
     },
     trash: {
         type: Boolean,
+        required: false,
         default: false
     },
+    slug: {
+        type: String,
+        min: 3,
+        max: 255,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    manufacturers_id: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Manufacturer'
+    }],
     timestamp: {
         type: Date,
-        default: Date.now
+        default: new Date().getTime()
     }
 });
+const CategorySchema = mongoose.model('Category', MongoCategorySchema);
+
+export class Category {
+    static createCategory = async (category: any): Promise<Document> => {
+        const {name, slug, content, status} = category;
+
+        const newCategorySchema = new CategorySchema({
+            name,
+            slug,
+            content,
+            status
+        });
+
+        return await newCategorySchema.save();
+    };
+    static getByName = async (categoryName: string): Promise<Document> => {
+        return CategorySchema.findOne({name: categoryName});
+    };
+    static getById = async (categoryId: string): Promise<Document> => {
+        return CategorySchema.findById(categoryId);
+    };
+    static safeRemove = async (categoryId: string): Promise<Document> => {
+        const category = await CategorySchema.findById(categoryId);
+
+        if (category) {
+            category.set('status', false);
+            return category;
+        } else {
+            throw new Error('Category not found');
+        }
+    };
+    static remove = async (categoryId: string): Promise<Document> => {
+        try {
+            return await CategorySchema.findByIdAndRemove(categoryId);
+        } catch (e) {
+            throw new Error(e);
+        }
+    };
+}
 
 
-export const Category = mongoose.model('Category', CategorySchema);
